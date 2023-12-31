e0665de876f541003d3b3b83fd111c241968bad7
EdA20 <ed9119@mail.ru>
2023-12-31 12:12:48 +0300

    hydra config added

--- image_classification/train.py
+++ image_classification/train.py
@@ -1,13 +1,21 @@
+import subprocess
+from pathlib import Path
+
+import hydra
 import numpy as np
 import torch
+from dataloader import get_cifar10_data
+from omegaconf import DictConfig
 from tqdm import tqdm
+from utils import instantiate
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+conf_dir = str(Path(__file__).resolve().parent.parent / "conf")
 
 
-def test(model, criterion, loader):
+def val_epoch(model, criterion, loader):
     loss_log = []
-    acc_log = []
+    acc_list = []
     model.eval()
 
     for images, labels in tqdm(loader):
@@ -19,9 +27,9 @@ def test(model, criterion, loader):
         loss_log.append(loss.item())
 
         acc = (logits.argmax(dim=1) == labels).float().mean()
-        acc_log.append(acc.item())
+        acc_list.append(acc.item())
 
-    return np.mean(loss_log), np.mean(acc_log)
+    return np.mean(loss_log), np.mean(acc_list)
 
 
 def train_epoch(model, optimizer, criterion, train_loader):
@@ -47,14 +55,21 @@ def train_epoch(model, optimizer, criterion, train_loader):
     return loss_log, acc_log
 
 
-def train(
-    model, optimizer, criterion, n_epochs, train_loader, val_loader, scheduler=None
-):
+@hydra.main(version_base=None, config_path=conf_dir, config_name="train_cfg")
+def train(cfg: DictConfig):
+    train_loader, val_loader = get_cifar10_data(train=True)
     train_loss_log, train_acc_log, val_loss_log, val_acc_log = [], [], [], []
+
+    model = instantiate(cfg.model.class_name)()
+    n_epochs = cfg.n_epochs
+    optimizer = instantiate(cfg.optimizer.class_name)(
+        model.parameters(), **cfg.optimizer.optimizer_params
+    )
+    criterion = instantiate(cfg.criterion.class_name)()
 
     for epoch in range(n_epochs):
         train_loss, train_acc = train_epoch(model, optimizer, criterion, train_loader)
-        val_loss, val_acc = test(model, criterion, val_loader)
+        val_loss, val_acc = val_epoch(model, criterion, val_loader)
 
         train_loss_log.extend(train_loss)
         train_acc_log.extend(train_acc)
@@ -66,7 +81,20 @@ def train(
         print(f" train loss: {np.mean(train_loss)}, train acc: {np.mean(train_acc)}")
         print(f" val loss: {val_loss}, val acc: {val_acc}\n")
 
-        if scheduler is not None:
-            scheduler.step()
+    torch.save(model, "model.pth")
 
-    return train_loss_log, train_acc_log, val_loss_log, val_acc_log
+
+def main():
+    subprocess.run(
+        [
+            "dvc",
+            "get",
+            "https://github.com/EdA20/MLops-image-classification",
+            "data/cifar-10-batches-py",
+        ]
+    )
+    train()
+
+
+if __name__ == "__main__":
+    main()
