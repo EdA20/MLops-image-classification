45f7ffb57520017000285162d8ca463d7dc0ca18
EdA20 <ed9119@mail.ru>
2024-01-01 15:47:29 +0300

    hydra config hyperparameter

--- image_classification/dataloader.py
+++ image_classification/dataloader.py
@@ -1,11 +1,16 @@
 from dataclasses import dataclass, field
+from pathlib import Path
 from typing import Tuple
 
 import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from omegaconf import DictConfig
 from sklearn.model_selection import train_test_split
+from utils import instantiate
+
+conf_dir = str(Path(__file__).resolve().parent.parent / "conf")
 
 
 @dataclass
@@ -26,13 +31,22 @@ class Cnst:
     )
 
 
-def get_cifar10_data(batch_size=64, train=True):
+def get_cifar10_data(cfg: DictConfig, train: bool):
     torch.manual_seed(0)
     np.random.seed(0)
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    )
+    transformations = []
+    transforms_yaml = cfg.transforms
+    transforms_params = cfg.transforms_params
+    for name, transform in transforms_yaml.items():
+        params = transforms_params.get(name)
+        if params is not None:
+            transform = instantiate(transform)(**params)
+        else:
+            transform = instantiate(transform)()
+        transformations.append(transform)
+
+    transform = transforms.Compose(transformations)
     # Загружаем данные
     set = torchvision.datasets.CIFAR10(root="./", train=train, transform=transform)
 
@@ -40,21 +54,15 @@ def get_cifar10_data(batch_size=64, train=True):
     # так что валидацию дополнительно выделяем из обучающей выборки
     if train:
         train_idx, valid_idx = train_test_split(
-            np.arange(len(set)), test_size=0.3, shuffle=True, random_state=0
+            np.arange(len(set)), **cfg.train_test_split
         )
         trainset = torch.utils.data.Subset(set, train_idx)
         valset = torch.utils.data.Subset(set, valid_idx)
 
-        train_loader = torch.utils.data.DataLoader(
-            trainset, batch_size=batch_size, shuffle=True, num_workers=2
-        )
-        val_loader = torch.utils.data.DataLoader(
-            valset, batch_size=batch_size, shuffle=False, num_workers=2
-        )
+        train_loader = torch.utils.data.DataLoader(trainset, **cfg.dataloader)
+        val_loader = torch.utils.data.DataLoader(valset, **cfg.dataloader)
         return train_loader, val_loader
 
     else:
-        test_loader = torch.utils.data.DataLoader(
-            set, batch_size=batch_size, shuffle=False, num_workers=2
-        )
+        test_loader = torch.utils.data.DataLoader(set, **cfg.dataloader)
         return test_loader
