b9e2b7fb99506844d371941b3aa8d0b552562e54
EdA20 <ed9119@mail.ru>
2024-01-05 17:08:58 +0300

    push csv file

--- image_classification/infer.py
+++ image_classification/infer.py
@@ -1,4 +1,7 @@
 import csv
+import os
+import subprocess
+from datetime import datetime
 from pathlib import Path
 
 import hydra
@@ -6,6 +9,8 @@ import torch
 from dataloader import Cnst, get_cifar10_data
 from omegaconf import DictConfig
 from tqdm import tqdm
+
+date = datetime.today().strftime("%Y-%m-%d")
 
 conf_dir = str(Path(__file__).resolve().parent.parent / "conf/dataloader")
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -31,10 +36,21 @@ def main(cfg: DictConfig):
             pred = [[CONSTANTS.classes[i]] for i in pred]
             preds.extend(pred)
 
-    with open("preds.csv", "w", newline="") as f:
+    output_dir = Path(__file__).resolve().parent.parent / f"outputs/{date}"
+    child = os.listdir(output_dir)[-1]
+    file = str(output_dir / child / "predictions.csv")
+
+    with open(file, "w", newline="") as f:
         writer = csv.writer(f)
         for row in preds:
             writer.writerow(row)
+
+    subprocess.run(["dvc", "add", file])
+    subprocess.run(["dvc", "push"])
+    subprocess.run(["git", "add", f"{str(file) + '.dvc'}"])
+    subprocess.run(
+        ["git", "commit", "-m", f"'predictions.csv added ({date + ' ' + child})'"]
+    )
 
 
 if __name__ == "__main__":
